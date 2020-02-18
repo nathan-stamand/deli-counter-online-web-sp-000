9e72ab08d9dc0892c3976c843470b4d7198dc28b
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-18 13:02:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,15 @@
 def line(array)
-  
+  if array.length == 0 
+    return "The line is currently empty."
+  else
+    line_number = []
+    i = 0 
+    array.each do |name|
+      line_number.push(" #{i+1}. #{array[i]}")
+      i += 1 
+    end
+    puts "The line is currently:#{line_number.join}"
+  end
 end
 
 def take_a_number(array, name)
